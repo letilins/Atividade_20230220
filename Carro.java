a8af3ada6bd5272f1736fcb746e243b097f27ce0
Letícia Lins <leticialins.rj@gmail.com>
2023-02-23 20:45:09 -0300

    Atividade_Java_20230220

--- Carro.java
+++ Carro.java
@@ -26,11 +26,11 @@ class Carro{
     }
 
     public void Acelerar(){
-        VelocidadeAtual = VelocidadeAtual + 10;
+        VelocidadeAtual += 10;
     }
 
     public void Frear(){
-         VelocidadeAtual = VelocidadeAtual - 10;
+         VelocidadeAtual -= 10;
     } 
 
 
@@ -38,7 +38,7 @@ public void imprimirInformacoes() {
             System.out.println("A Marca do carro é " + Marca );
             System.out.println("O modelo do carro é " + Modelo );
             System.out.println("O ano da fabricação do carro é " + AnoFabricacao );
-            System.out.println("A velocidade do meu carro é:" + VelocidadeAtual );   
+            System.out.println("A velocidade do meu carro é:" + VelocidadeAtual + " Km" );   
 
             
       
